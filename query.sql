417f2d41f5810e44e72424566f611b5cac1407fa
Iryna Babych <wilyreptiloid@gmail.com>
2023-01-06 13:32:06 +0200

    Finished query

--- query.sql
+++ query.sql
@@ -1,16 +1,14 @@
-SELECT * FROM islands
-
 
 -- 1
 -- Скільки пінгвінів було досліджено на кожному острові?
--- SELECT island_name, COUNT(sample_number) as penguin_quantity FROM penguinrecords
--- GROUP BY island_name
+SELECT island_name, COUNT(sample_number) as penguin_quantity FROM penguinrecords
+GROUP BY island_name
 
 -- 2
 -- Яка частина пінгвінів мала зафіксовану цілу кладку (тобто, 2 яйця в гнізді)?
--- SELECT clutch_completion, COUNT(sample_number) AS penguin_quantity FROM penguinrecords
--- GROUP BY clutch_completion
+SELECT clutch_completion, COUNT(sample_number) AS penguin_quantity FROM penguinrecords
+GROUP BY clutch_completion
 
--- 3 Який взаємозв'язок між довжино
--- SELECT culmen_length, body_mass FROM penguinrecords
--- WHERE species = 'Adelie Penguin (Pygoscelis adeliae)'
+-- 3 Який взаємозв'язок між довжиною клюва та масою пінгвінів Аделі?
+SELECT culmen_length, body_mass FROM penguinrecords
+WHERE species = 'Adelie Penguin (Pygoscelis adeliae)'
